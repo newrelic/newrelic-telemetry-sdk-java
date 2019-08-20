154c9ffc815058404106b8ac9962ecd945b7b932
Xi Xia <triplextension@gmail.com>
2019-08-20 11:15:07 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ listOf(":metrics", ":telemetry", ":telemetry-components").forEach {
                             password = project.properties["localSonatypePassword"] as String?
                         }
                     }
-                    else{
+                    else {
                         val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
                         val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
                         url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
