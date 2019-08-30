047be824ba128ef895082b4f532cb40f85656b6e
jplumb <jplumb@newrelic.com>
2019-08-30 15:41:51 -0700

    build out MBJTBWTest and delete MBJsonTest

--- metrics/src/test/java/com/newrelic/telemetry/json/MetricBatchJsonTelemetryBlockWriterTest.java
+++ metrics/src/test/java/com/newrelic/telemetry/json/MetricBatchJsonTelemetryBlockWriterTest.java
@@ -1,11 +1,20 @@
 package com.newrelic.telemetry.json;
 
-class MetricBatchJsonTest {
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-  //TODO: FIXME DELME
+import com.newrelic.telemetry.Attributes;
+import com.newrelic.telemetry.Gauge;
+import com.newrelic.telemetry.MetricBatch;
+import java.util.Collections;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
-  /*private MetricToJson metricToJson;
-  private AttributesJson attributeJson;
+class MetricBatchJsonTelemetryBlockWriterTest {
+
+  private MetricToJson metricToJson;
   private Gauge gauge;
   private Attributes commonAttributes;
   private MetricBatch metricBatch;
@@ -16,20 +25,6 @@ class MetricBatchJsonTest {
     gauge = new Gauge("gauge", 3d, 555, new Attributes());
     metricBatch = new MetricBatch(Collections.singletonList(gauge), commonAttributes);
     metricToJson = mock(MetricToJson.class);
-    attributeJson = mock(AttributesJson.class);
-  }
-
-  @Test
-  @DisplayName("Formatting with common attributes is structured correctly")
-  void testCommonJsonBlock() throws Exception {
-    String expectedCommonJsonBlock = "\"common\":{\"attributes\":{\"key\":\"val\"}}";
-    when(attributeJson.toJson(commonAttributes.asMap())).thenReturn("{\"key\":\"val\"}");
-
-    StringBuilder stringBuilder = new StringBuilder();
-    MetricBatchJson metricBatchJson = new MetricBatchJson(metricToJson, attributeJson);
-    metricBatchJson.appendCommonJson(metricBatch, stringBuilder);
-
-    JSONAssert.assertEquals(expectedCommonJsonBlock, stringBuilder.toString(), false);
   }
 
   @Test
@@ -41,9 +36,10 @@ class MetricBatchJsonTest {
         .thenReturn("{\"name\":\"gauge\",\"type\":\"gauge\",\"value\":3.0,\"timestamp\":555}");
 
     StringBuilder stringBuilder = new StringBuilder();
-    MetricBatchJson metricBatchJson = new MetricBatchJson(metricToJson, attributeJson);
-    metricBatchJson.appendTelemetry(metricBatch, stringBuilder);
+    MetricBatchJsonTelemetryBlockWriter testClass = new MetricBatchJsonTelemetryBlockWriter(
+        metricToJson);
+    testClass.appendTelemetry(metricBatch, stringBuilder);
 
     JSONAssert.assertEquals(expectedTelemetryJsonBlock, stringBuilder.toString(), false);
-  }*/
+  }
 }
