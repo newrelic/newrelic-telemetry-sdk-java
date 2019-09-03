084ad492210667b28a233310c0e20bd94c928e7d
jplumb <jplumb@newrelic.com>
2019-09-03 09:45:37 -0700

    build out tests

--- metrics/src/test/java/com/newrelic/telemetry/json/TypeDispatchingJsonCommonBlockWriterTest.java
+++ metrics/src/test/java/com/newrelic/telemetry/json/TypeDispatchingJsonCommonBlockWriterTest.java
@@ -1,16 +1,13 @@
 package com.newrelic.telemetry.json;
 
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import com.newrelic.telemetry.Attributes;
-import com.newrelic.telemetry.Gauge;
 import com.newrelic.telemetry.Metric;
+import com.newrelic.telemetry.Span;
 import com.newrelic.telemetry.Telemetry.Type;
 import com.newrelic.telemetry.TelemetryBatch;
-import java.util.Collection;
-import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
 class TypeDispatchingJsonCommonBlockWriterTest {
@@ -18,12 +15,10 @@ class TypeDispatchingJsonCommonBlockWriterTest {
   @Test
   void testMetrics() {
     StringBuilder sb = new StringBuilder();
-    Collection<Metric> telemetry = Collections
-        .singleton(new Gauge("one", 1, 1, new Attributes().put("foo", "bar")));
-    TelemetryBatch<Metric> batch = new TelemetryBatch<>(Type.METRIC, telemetry,
-        new Attributes().put("bar", "baz"));
-
+    TelemetryBatch<Metric> batch = mock(TelemetryBatch.class);
     JsonCommonBlockWriter metricsWriter = mock(JsonCommonBlockWriter.class);
+
+    when(batch.getType()).thenReturn(Type.METRIC);
 
     TypeDispatchingJsonCommonBlockWriter testClass = new TypeDispatchingJsonCommonBlockWriter(
         metricsWriter, null);
@@ -34,7 +29,18 @@ class TypeDispatchingJsonCommonBlockWriterTest {
 
   @Test
   void testSpans() {
-    fail("build me");
+    StringBuilder sb = new StringBuilder();
+    TelemetryBatch<Span> batch = mock(TelemetryBatch.class);
+    JsonCommonBlockWriter spansWriter = mock(JsonCommonBlockWriter.class);
+
+    when(batch.getType()).thenReturn(Type.SPAN);
+
+    TypeDispatchingJsonCommonBlockWriter testClass = new TypeDispatchingJsonCommonBlockWriter(
+        null, spansWriter);
+
+    testClass.appendCommonJson(batch, sb);
+    verify(spansWriter).appendCommonJson(batch, sb);
+
   }
 
 }
--- metrics/src/test/java/com/newrelic/telemetry/json/TypeDispatchingJsonTelemetryBlockWriterTest.java
+++ metrics/src/test/java/com/newrelic/telemetry/json/TypeDispatchingJsonTelemetryBlockWriterTest.java
@@ -1,19 +1,45 @@
 package com.newrelic.telemetry.json;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.newrelic.telemetry.Metric;
+import com.newrelic.telemetry.Span;
+import com.newrelic.telemetry.Telemetry.Type;
+import com.newrelic.telemetry.TelemetryBatch;
 import org.junit.jupiter.api.Test;
 
 class TypeDispatchingJsonTelemetryBlockWriterTest {
 
   @Test
   void testMetrics() {
-    fail("build me");
+    StringBuilder sb = new StringBuilder();
+    TelemetryBatch<Metric> batch = mock(TelemetryBatch.class);
+    JsonTelemetryBlockWriter metricsWriter = mock(JsonTelemetryBlockWriter.class);
+
+    when(batch.getType()).thenReturn(Type.METRIC);
+
+    TypeDispatchingJsonTelemetryBlockWriter testClass = new TypeDispatchingJsonTelemetryBlockWriter(
+        metricsWriter, null);
+
+    testClass.appendTelemetry(batch, sb);
+    verify(metricsWriter).appendTelemetry(batch, sb);
   }
 
   @Test
   void testSpans() {
-    fail("build me");
+    StringBuilder sb = new StringBuilder();
+    TelemetryBatch<Span> batch = mock(TelemetryBatch.class);
+    JsonTelemetryBlockWriter spansWriter = mock(JsonTelemetryBlockWriter.class);
+
+    when(batch.getType()).thenReturn(Type.SPAN);
+
+    TypeDispatchingJsonTelemetryBlockWriter testClass = new TypeDispatchingJsonTelemetryBlockWriter(
+        null, spansWriter);
+
+    testClass.appendTelemetry(batch, sb);
+    verify(spansWriter).appendTelemetry(batch, sb);
   }
 
 }
