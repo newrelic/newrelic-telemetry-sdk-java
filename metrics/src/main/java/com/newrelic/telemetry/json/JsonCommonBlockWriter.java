86cf1b65a9918f4848b6c14d01f5390e93de9327
jplumb <jplumb@newrelic.com>
2019-09-03 10:47:58 -0700

    add some javadocs

--- metrics/src/main/java/com/newrelic/telemetry/json/JsonCommonBlockWriter.java
+++ metrics/src/main/java/com/newrelic/telemetry/json/JsonCommonBlockWriter.java
@@ -3,6 +3,11 @@ package com.newrelic.telemetry.json;
 import com.newrelic.telemetry.Telemetry;
 import com.newrelic.telemetry.TelemetryBatch;
 
+/**
+ * Implementations of this class are responsible for converting a telemetry batch into JSON
+ * fragments. This particular writer is responsible for writing the "common" block for a single
+ * batch.
+ */
 public interface JsonCommonBlockWriter {
 
   <T extends Telemetry> void appendCommonJson(TelemetryBatch<T> batch, StringBuilder builder);
