a64edebc5c2cec25a4f80ab6758d3965c7c49bf1
Xi Xia <xxia@newrelic.com>
2019-09-05 16:04:38 -0700

    google format test

--- integration_test/src/test/java/com/newrelic/telemetry/SpanApiIntegrationTest.java
+++ integration_test/src/test/java/com/newrelic/telemetry/SpanApiIntegrationTest.java
@@ -15,7 +15,6 @@ import com.newrelic.telemetry.spans.SpanBatchSender;
 import java.net.URI;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -34,7 +33,7 @@ import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-public class SpanApiIntegrationTest {
+class SpanApiIntegrationTest {
 
   private static final int SERVICE_PORT = 1080 + new Random().nextInt(900);
   private static String containerIpAddress;
@@ -58,58 +57,68 @@ public class SpanApiIntegrationTest {
   @BeforeEach
   void setUp() throws Exception {
     mockServerClient.reset();
-    spanBatchSender = SpanBatchSender.builder("fakeKey", new OkHttpPoster(Duration.ofMillis(1500)),
-        new AttributesGson(new Gson()))
-        .uriOverride(URI.create("http://" + containerIpAddress + ":" + SERVICE_PORT))
-        .enableAuditLogging()
-        .build();
+    spanBatchSender =
+        SpanBatchSender.builder()
+            .apiKey("fakeKey")
+            .httpPoster(new OkHttpPoster(Duration.ofMillis(1500)))
+            .attributesJson(new AttributesGson(new Gson()))
+            .uriOverride(URI.create("http://" + containerIpAddress + ":" + SERVICE_PORT))
+            .enableAuditLogging()
+            .build();
   }
 
   @Test
   @DisplayName("Low Level SDK can send a span batch and returns a successful response")
-  void testSuccessfulSpanSend() throws Exception{
-    //given
+  void testSuccessfulSpanSend() throws Exception {
     SpanPayload expectedPayload =
         new SpanPayload(
-            ImmutableMap.of("trace.id", "123456","attributes", singletonMap("key1", "val1")),
-            Arrays.asList(
+            ImmutableMap.of("trace.id", "123456", "attributes", singletonMap("key1", "val1")),
+            singletonList(
                 ImmutableMap.<String, Object>builder()
                     .put("id", "6f377f46-3f10-4e8e-8390-7717044cdbe9")
                     .put("trace.id", "123456")
-                    .put("timestamp", 55555 )
-                    .put("attributes", ImmutableMap.of("duration.ms", 60, "service.name","Span Test Service","name", "spanTest"))
-                    .build()
-            )
-        );
+                    .put("timestamp", 55555)
+                    .put(
+                        "attributes",
+                        ImmutableMap.of(
+                            "duration.ms",
+                            60,
+                            "service.name",
+                            "Span Test Service",
+                            "name",
+                            "spanTest"))
+                    .build()));
 
-    //When
-    mockServerClient.when(new HttpRequest()
-        .withMethod("POST")
-        .withPath("/trace/v1")
-        .withBody(
-            json(
-                new SpanPayload[] {expectedPayload},
-                MediaType.JSON_UTF_8,
-                MatchType.ONLY_MATCHING_FIELDS))
-        .withHeader("User-Agent", "NewRelic-Java-TelemetrySDK/.*")
-        .withHeader("Content-Type", "application/json; charset=utf-8")
-        .withHeader("Content-Length", ".*"))
+    mockServerClient
+        .when(
+            new HttpRequest()
+                .withMethod("POST")
+                .withPath("/trace/v1")
+                .withBody(
+                    json(
+                        new SpanPayload[] {expectedPayload},
+                        MediaType.JSON_UTF_8,
+                        MatchType.ONLY_MATCHING_FIELDS))
+                .withHeader("User-Agent", "NewRelic-Java-TelemetrySDK/.*")
+                .withHeader("Content-Type", "application/json; charset=utf-8")
+                .withHeader("Content-Length", ".*"))
         .respond(new HttpResponse().withStatusCode(202));
 
-    //then
-
     List<Span> spans = new ArrayList<>();
-    spans.add(Span.builder("6f377f46-3f10-4e8e-8390-7717044cdbe9")
-    .traceId("123456")
-    .name("spanTest")
-    .durationMs(60)
-    .timestamp(55555)
-    .serviceName("Span Test Service").build());
-    Response response = spanBatchSender.sendBatch(new SpanBatch(spans, getCommonAttributes(), "123456"));
+    spans.add(
+        Span.builder("6f377f46-3f10-4e8e-8390-7717044cdbe9")
+            .traceId("123456")
+            .name("spanTest")
+            .durationMs(60)
+            .timestamp(55555)
+            .serviceName("Span Test Service")
+            .build());
+    Response response =
+        spanBatchSender.sendBatch(new SpanBatch(spans, getCommonAttributes(), "123456"));
 
     assertEquals(202, response.getStatusCode());
-    assertEquals("Accepted", response.getStatusMessage());  }
-
+    assertEquals("Accepted", response.getStatusMessage());
+  }
 
   private static Attributes getCommonAttributes() {
     return new Attributes().put("key1", "val1");
