c6ed798f74d7709dd86ec7f6ec0db67ed4daf8ba
jwatson <jwatson@newrelic.com>
2019-09-06 13:14:59 -0700

    document the OkHttpPoster and add a constructor that takes the underlying OkHttpClient

--- telemetry-http-okhttp/src/main/java/com/newrelic/telemetry/OkHttpPoster.java
+++ telemetry-http-okhttp/src/main/java/com/newrelic/telemetry/OkHttpPoster.java
@@ -19,11 +19,18 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 
+/** Implementation of the HttpPoster interface using an OkHttp client. */
 public class OkHttpPoster implements HttpPoster {
   private final OkHttpClient okHttpClient;
 
+  /** Create an OkHttpPoster with a default OkHttpClient, with only a custom call timeout. */
   public OkHttpPoster(Duration callTimeout) {
-    this.okHttpClient = new OkHttpClient.Builder().callTimeout(callTimeout).build();
+    this(new OkHttpClient.Builder().callTimeout(callTimeout).build());
+  }
+
+  /** Create an OkHttpPoster with your own OkHttpClient implementation. */
+  public OkHttpPoster(OkHttpClient client) {
+    this.okHttpClient = client;
   }
 
   @Override
