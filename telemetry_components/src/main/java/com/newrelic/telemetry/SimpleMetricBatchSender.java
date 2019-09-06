b1af95a3b4b340bcf5edf0586e780330e945c2f0
jplumb <jplumb@newrelic.com>
2019-09-06 09:55:38 -0700

    add simple build methods to just get an instance

--- telemetry_components/src/main/java/com/newrelic/telemetry/SimpleMetricBatchSender.java
+++ telemetry_components/src/main/java/com/newrelic/telemetry/SimpleMetricBatchSender.java
@@ -14,8 +14,16 @@ import java.time.Duration;
 
 public class SimpleMetricBatchSender {
 
+  public static MetricBatchSender build(String apiKey) {
+    return builder(apiKey).build();
+  }
+
   public static MetricBatchSenderBuilder builder(String apiKey) {
     return builder(apiKey, Duration.ofSeconds(2));
+  }
+
+  public static MetricBatchSender build(String apiKey, Duration callTimeout) {
+    return builder(apiKey, callTimeout).build();
   }
 
   public static MetricBatchSenderBuilder builder(String apiKey, Duration callTimeout) {
