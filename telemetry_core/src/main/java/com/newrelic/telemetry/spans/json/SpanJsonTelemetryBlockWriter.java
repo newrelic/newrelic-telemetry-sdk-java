8f2f5946caa80c25c2b943b574c5fddfec37d809
jplumb <jplumb@newrelic.com>
2019-10-01 10:07:01 -0700

    discovered `putIfAbsent()` in the future!

--- telemetry_core/src/main/java/com/newrelic/telemetry/spans/json/SpanJsonTelemetryBlockWriter.java
+++ telemetry_core/src/main/java/com/newrelic/telemetry/spans/json/SpanJsonTelemetryBlockWriter.java
@@ -4,8 +4,6 @@
  */
 package com.newrelic.telemetry.spans.json;
 
-import static java.util.Optional.ofNullable;
-
 import com.google.gson.stream.JsonWriter;
 import com.newrelic.telemetry.json.AttributesJson;
 import com.newrelic.telemetry.spans.Span;
@@ -14,8 +12,6 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Supplier;
 import lombok.Value;
 
 @Value
@@ -35,8 +31,7 @@ public class SpanJsonTelemetryBlockWriter {
           jsonWriter.name("trace.id").value(span.getTraceId());
         }
         jsonWriter.name("timestamp").value(span.getTimestamp());
-        Map<String, Object> enhancedAttributes = new AttributesEnhancer(span).enhance();
-        jsonWriter.name("attributes").jsonValue(attributesJson.toJson(enhancedAttributes));
+        jsonWriter.name("attributes").jsonValue(attributesJson.toJson(enhanceAttributes(span)));
         jsonWriter.endObject();
       }
       jsonWriter.endArray();
@@ -45,34 +40,15 @@ public class SpanJsonTelemetryBlockWriter {
     }
   }
 
-  private static class AttributesEnhancer {
-
-    private final Span span;
-    private final Map<String, Object> newAttrs;
-
-    AttributesEnhancer(Span span) {
-      this.span = span;
-      this.newAttrs = new HashMap<>(span.getAttributes().asMap());
+  private Map<String, Object> enhanceAttributes(Span span) {
+    Map<String, Object> result = new HashMap<>(span.getAttributes().asMap());
+    result.putIfAbsent("name", span.getName());
+    result.putIfAbsent("parent.id", span.getParentId());
+    result.putIfAbsent("duration.ms", span.getDurationMs());
+    result.putIfAbsent("service.name", span.getServiceName());
+    if (span.isError()) {
+      result.put("error", true);
     }
-
-    Map<String, Object> enhance() {
-      put("name", () -> ofNullable(span.getName()));
-      put("parent.id", () -> ofNullable(span.getParentId()));
-      put("duration.ms", () -> ofNullable(span.getDurationMs()));
-      put("service.name", () -> ofNullable(span.getServiceName()));
-      if (span.isError()) {
-        newAttrs.put("error", true);
-      }
-      return newAttrs;
-    }
-
-    private Object getAttr(String name) {
-      return span.getAttributes().asMap().get(name);
-    }
-
-    private void put(String key, Supplier<Optional<Object>> sup1) {
-      newAttrs.put(key, sup1.get().orElse(getAttr(key)));
-    }
-
+    return result;
   }
 }
