c299484edc556c4df47a23ffd752592a1eb17156
Ben Evans <bevans@newrelic.com>
2020-02-26 16:58:59 +0100

    Add test for split-sending

--- telemetry_core/src/test/java/com/newrelic/telemetry/events/EventBatchSenderTest.java
+++ telemetry_core/src/test/java/com/newrelic/telemetry/events/EventBatchSenderTest.java
@@ -1,17 +1,43 @@
 package com.newrelic.telemetry.events;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 import com.newrelic.telemetry.Attributes;
 import com.newrelic.telemetry.Response;
+import com.newrelic.telemetry.events.json.EventBatchMarshaller;
+import com.newrelic.telemetry.exceptions.RetryWithSplitException;
+import com.newrelic.telemetry.transport.BatchDataSender;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class EventBatchSenderTest {
 
   @Test
-  void testSimpleSend() throws Exception {
-    Event el;
+  void testSplitSend() throws Exception {
+    Event el = new Event("xxx", null, System.currentTimeMillis(), "svcA", "A");
+    Event el2 = new Event("yyy", null, System.currentTimeMillis(), "svcA", "A");
+    List<Event> events = new ArrayList<>();
+    events.add(el);
+    events.add(el2);
+    EventBatch batch = new EventBatch(events, new Attributes().put("j", "k"));
+
+    String json = "{a great document}";
+    EventBatchMarshaller marshaller = mock(EventBatchMarshaller.class);
+    when(marshaller.toJson(any())).thenReturn(json);
+
+    Response ok = new Response(200, "OK", "yup");
+    BatchDataSender sender = mock(BatchDataSender.class);
+    when(sender.send(json)).thenThrow(RetryWithSplitException.class).thenReturn(ok);
+
+    EventBatchSender testClass = new EventBatchSender(marshaller, sender);
+
+    Response result = testClass.sendBatch(batch);
+    assertEquals(ok, result);
+    verify(sender, times(3)).send(any());
   }
 
   @Test
