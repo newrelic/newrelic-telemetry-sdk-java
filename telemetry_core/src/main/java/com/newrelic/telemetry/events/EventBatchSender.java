fdabec35a39a928417ceaa016de487811dea7cec
Ben Evans <bevans@newrelic.com>
2020-02-26 15:03:02 +0100

    Add first cut of splitting sender

--- telemetry_core/src/main/java/com/newrelic/telemetry/events/EventBatchSender.java
+++ telemetry_core/src/main/java/com/newrelic/telemetry/events/EventBatchSender.java
@@ -1,11 +1,19 @@
 package com.newrelic.telemetry.events;
 
 import com.newrelic.telemetry.Response;
+import com.newrelic.telemetry.TelemetryBatch;
 import com.newrelic.telemetry.events.json.EventBatchMarshaller;
 import com.newrelic.telemetry.exceptions.ResponseException;
+import com.newrelic.telemetry.exceptions.RetryWithSplitException;
 import com.newrelic.telemetry.transport.BatchDataSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class EventBatchSender {
 
@@ -39,9 +47,53 @@ public class EventBatchSender {
       return new Response(202, "Ignored", "Empty batch");
     }
     logger.debug(
-        "Sending an event batch (number of metrics: {}) to the New Relic event ingest endpoint)",
+        "Sending an event batch (number of events: {}) to the New Relic event ingest endpoint)",
         batch.size());
     String json = marshaller.toJson(batch);
-    return sender.send(json);
+
+    Response response = null;
+    try {
+      response = sender.send(json);
+    } catch (RetryWithSplitException splitx) {
+       response = splitBatchAndSend(batch, new LinkedBlockingDeque<>());
+    }
+
+    return response;
+  }
+
+  @SuppressWarnings("unchecked")
+  Response splitBatchAndSend(EventBatch batch, BlockingDeque<TelemetryBatch> queue) {
+    logger.info(
+            "Tried to send a too-large event batch (number of events: {}) to the New Relic event ingest endpoint. Splitting)",
+            batch.size()
+    );
+    List<TelemetryBatch<Event>> twoBatches = batch.split();
+    Response response = null;
+
+    // Preserve in-order processing
+    queue.addFirst(twoBatches.get(1));
+    queue.addFirst(twoBatches.get(0));
+    while (!queue.isEmpty()) {
+      final EventBatch eb = (EventBatch) queue.pollFirst();
+      final String json = marshaller.toJson(eb);
+
+      try {
+        response = sender.send(json);
+      } catch (RetryWithSplitException splitx) {
+        response = splitBatchAndSend(eb, queue);
+      } catch (ResponseException rsx) {
+        // We have to log and swallow this exception as there may be other split batches
+        // might succeed at sending
+        logger.info(
+                "Failed to send a split event batch to the New Relic event ingest endpoint. Exception: {}",
+                rsx
+        );
+      }
+    }
+    if (response == null) {
+      response = new Response(200, "OK", "Large payload was split - check log in case of dropped sub-batch");
+    }
+
+    return response;
   }
 }
