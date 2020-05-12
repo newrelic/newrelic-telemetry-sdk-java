2f142f3b330689fbc16729b58ea26e969db4973c
Jeff Alder <49817386+jeffalder@users.noreply.github.com>
2020-05-12 13:08:51 -0700

    Best to use new methods for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-buildscript {
-    dependencies {
-        classpath("gradle.plugin.com.github.sherter.google-java-format:google-java-format-gradle-plugin:0.8")
-    }
-}
+import com.newrelic.telemetry.gradle.configureRepositories
+import com.newrelic.telemetry.gradle.configuredPom
 
 plugins {
     java
@@ -65,52 +62,10 @@ listOf(":telemetry", ":telemetry-http-okhttp", ":telemetry-http-java11").forEach
                     from(components["java"])
                     artifact(tasks["sourcesJar"])
                     artifact(tasks["javadocJar"])
-                    pom {
-                        name.set(project.name)
-                        description.set("Used to send telemetry data to New Relic")
-                        url.set("https://github.com/newrelic/newrelic-telemetry-sdk-java")
-                        licenses {
-                            license {
-                                name.set("The Apache License, Version 2.0")
-                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                                distribution.set("repo")
-                            }
-                        }
-                        developers {
-                            developer {
-                                id.set("newrelic")
-                                name.set("New Relic")
-                                email.set("opensource@newrelic.com")
-                            }
-                        }
-                        scm {
-                            url.set("git@github.com:newrelic/newrelic-telemetry-sdk-java.git")
-                            connection.set("scm:git:git@github.com:newrelic/newrelic-telemetry-sdk-java.git")
-                        }
-                    }
+                    configuredPom(project)
                 }
             }
-            repositories {
-                maven {
-                    if (useLocalSonatype) {
-                        val releasesRepoUrl = uri("http://localhost:8081/repository/maven-releases/")
-                        val snapshotsRepoUrl = uri("http://localhost:8081/repository/maven-snapshots/")
-                        url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-                    }
-                    else {
-                        val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                        val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-                        url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-                        configure<SigningExtension> {
-                            sign(publications["mavenJava"])
-                        }
-                    }
-                    credentials {
-                        username = project.properties["sonatypeUsername"] as String?
-                        password = project.properties["sonatypePassword"] as String?
-                    }
-                }
-            }
+            configureRepositories(project, useLocalSonatype, "mavenJava")
         }
     }
 }
