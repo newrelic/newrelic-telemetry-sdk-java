ca7e97e71c8c3bdb7959d2f5b5bbf8fa4d3370c8
ryu.ishikawa <ryu.ishikawa@paypay-corp.co.jp>
2020-05-28 19:58:44 +0900

    allow consumer to set shutdownSeconds

--- telemetry/src/main/java/com/newrelic/telemetry/TelemetryClient.java
+++ telemetry/src/main/java/com/newrelic/telemetry/TelemetryClient.java
@@ -46,7 +46,7 @@ public class TelemetryClient {
     thread.setDaemon(true);
     return thread;
   });
-  private final long shutdownSeconds = 3;
+  private final long shutdownSeconds;
 
   /**
    * Create a new TelemetryClient instance, with four senders. Note that if you don't intend to send
@@ -66,6 +66,28 @@ public class TelemetryClient {
     this.spanBatchSender = spanBatchSender;
     this.eventBatchSender = eventBatchSender;
     this.logBatchSender = logBatchSender;
+    this.shutdownSeconds = 3L;
+  }
+
+  /**
+   * Create a new TelemetryClient instance, with three senders and seconds to wait for shutdown.
+   *
+   * @param metricBatchSender The sender for dimensional metrics.
+   * @param spanBatchSender The sender for distributed tracing spans.
+   * @param eventBatchSender The sender for custom events
+   * @param shutdownSeconds num of seconds to wait for graceful shutdown of its executor
+   */
+  public TelemetryClient(
+      MetricBatchSender metricBatchSender,
+      SpanBatchSender spanBatchSender,
+      EventBatchSender eventBatchSender,
+      LogBatchSender logBatchSender,
+      long shutdownSeconds) {
+    this.metricBatchSender = metricBatchSender;
+    this.spanBatchSender = spanBatchSender;
+    this.eventBatchSender = eventBatchSender;
+    this.logBatchSender = logBatchSender;
+    this.shutdownSeconds = shutdownSeconds;
   }
 
   /**
