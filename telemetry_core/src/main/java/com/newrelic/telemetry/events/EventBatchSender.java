291bb1cec3c2caaa789374d022122464f4d93458
jplumb <jplumb@newrelic.com>
2020-07-22 14:19:39 -0700

    remove unused

--- telemetry_core/src/main/java/com/newrelic/telemetry/events/EventBatchSender.java
+++ telemetry_core/src/main/java/com/newrelic/telemetry/events/EventBatchSender.java
@@ -10,20 +10,16 @@ import com.newrelic.telemetry.EventBatchSenderFactory;
 import com.newrelic.telemetry.Response;
 import com.newrelic.telemetry.SenderConfiguration;
 import com.newrelic.telemetry.SenderConfiguration.SenderConfigurationBuilder;
-import com.newrelic.telemetry.TelemetryBatch;
 import com.newrelic.telemetry.events.json.EventBatchMarshaller;
 import com.newrelic.telemetry.exceptions.ResponseException;
-import com.newrelic.telemetry.exceptions.RetryWithSplitException;
 import com.newrelic.telemetry.http.HttpPoster;
 import com.newrelic.telemetry.transport.BatchDataSender;
 import com.newrelic.telemetry.util.Utils;
-import java.net.URL;
-import java.util.List;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.function.Supplier;
 
 public class EventBatchSender {
   private static final String EVENTS_PATH = "/v1/accounts/events";
@@ -60,42 +56,6 @@ public class EventBatchSender {
     String json = marshaller.toJson(batch);
 
     return sender.send(json);
-  }
-
-  @SuppressWarnings("unchecked")
-  Response splitBatchAndSend(EventBatch batch, BlockingDeque<TelemetryBatch> queue) {
-    logger.info(
-        "Tried to send a too-large event batch (number of events: {}) to the New Relic event ingest endpoint. Splitting)",
-        batch.size());
-    List<TelemetryBatch<Event>> twoBatches = batch.split();
-    Response response = null;
-
-    // Preserve in-order processing
-    queue.addFirst(twoBatches.get(1));
-    queue.addFirst(twoBatches.get(0));
-    while (!queue.isEmpty()) {
-      final EventBatch eb = (EventBatch) queue.pollFirst();
-      final String json = marshaller.toJson(eb);
-
-      try {
-        response = sender.send(json);
-      } catch (RetryWithSplitException splitx) {
-        response = splitBatchAndSend(eb, queue);
-      } catch (ResponseException rsx) {
-        // We have to log and swallow this exception as there may be other split batches
-        // might succeed at sending
-        logger.info(
-            "Failed to send a split event batch to the New Relic event ingest endpoint. Exception: {}",
-            rsx);
-      }
-    }
-    if (response == null) {
-      response =
-          new Response(
-              200, "OK", "Large payload was split - check log in case of dropped sub-batch");
-    }
-
-    return response;
   }
 
   /**
