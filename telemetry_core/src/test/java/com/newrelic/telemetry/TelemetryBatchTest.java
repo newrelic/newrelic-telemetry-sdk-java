91ce6be653dac5aa0d8ead55efbea89e40ef1005
jplumb <jplumb@newrelic.com>
2020-08-18 15:42:44 -0700

    verify split behavior

--- telemetry_core/src/test/java/com/newrelic/telemetry/TelemetryBatchTest.java
+++ telemetry_core/src/test/java/com/newrelic/telemetry/TelemetryBatchTest.java
@@ -11,9 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.newrelic.telemetry.metrics.Count;
+import com.newrelic.telemetry.metrics.Gauge;
 import com.newrelic.telemetry.metrics.Metric;
 import com.newrelic.telemetry.metrics.MetricBatch;
+
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class TelemetryBatchTest {
@@ -41,5 +46,18 @@ class TelemetryBatchTest {
     //2280a03c-9c2a-4527-ac32-1332e8de159c
     String regex = "^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$";
     assertTrue(batch.getUuid().toString().matches(regex));
+  }
+
+  @Test
+  void testSplitMakesNewUuid() {
+    long now = System.currentTimeMillis();
+    Metric m1 = new Gauge("foo1", 12.34, now, new Attributes());
+    Metric m2 = new Gauge("foo2", 56.78, now, new Attributes());
+    TelemetryBatch<Metric> batch = new MetricBatch(Arrays.asList(m1, m2), new Attributes());
+    List<TelemetryBatch<Metric>> split = batch.split();
+    assertEquals(2, split.size());
+    assertNotEquals(batch.getUuid(), split.get(0).getUuid());
+    assertNotEquals(batch.getUuid(), split.get(1).getUuid());
+    assertNotEquals(split.get(0).getUuid(), split.get(1).getUuid());
   }
 }
