86c1033dc809e9dee3e945cb99e7a05f999f0b01
jplumb <jplumb@newrelic.com>
2020-08-18 15:35:27 -0700

    add uuid to telemetry batch

--- telemetry_core/src/test/java/com/newrelic/telemetry/TelemetryBatchTest.java
+++ telemetry_core/src/test/java/com/newrelic/telemetry/TelemetryBatchTest.java
@@ -5,7 +5,9 @@
 package com.newrelic.telemetry;
 
 import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.newrelic.telemetry.metrics.Count;
@@ -24,5 +26,20 @@ class TelemetryBatchTest {
         new MetricBatch(Collections.singleton(metric), new Attributes());
     assertTrue(batch1.isEmpty());
     assertFalse(batch2.isEmpty());
+  }
+
+  @Test
+  void testHasUUID() {
+    TelemetryBatch<Metric> batch1 = new MetricBatch(emptyList(), new Attributes());
+    TelemetryBatch<Metric> batch2 = new MetricBatch(emptyList(), new Attributes());
+    assertNotEquals(batch1.getUuid(), batch2.getUuid());
+  }
+
+  @Test
+  void testUuidFormat() {
+    TelemetryBatch<Metric> batch = new MetricBatch(emptyList(), new Attributes());
+    //2280a03c-9c2a-4527-ac32-1332e8de159c
+    String regex = "^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$";
+    assertTrue(batch.getUuid().toString().matches(regex));
   }
 }
