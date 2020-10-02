2f958f9b9e03cce3faa19f54e523778d6af3613a
Schalk Cronje <ysb33r@gmail.com>
2020-10-02 17:58:41 +0200

    Converts settins.gradle to Kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "newrelic-telemetry-sdk-java"
+
+include (":telemetry")
+include (":telemetry-all")
+include (":telemetry_examples")
+include (":telemetry-core")
+include (":integration_test")
+include ("telemetry-http-okhttp")
+include ("telemetry-http-java11")
+
