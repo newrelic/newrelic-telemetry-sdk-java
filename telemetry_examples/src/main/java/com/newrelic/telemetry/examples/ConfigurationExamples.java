c5f29a0180adbf2e7dc590599463e63eda6e94b2
jplumb <jplumb@newrelic.com>
2020-11-03 14:15:22 -0800

    update example

--- telemetry_examples/src/main/java/com/newrelic/telemetry/examples/ConfigurationExamples.java
+++ telemetry_examples/src/main/java/com/newrelic/telemetry/examples/ConfigurationExamples.java
@@ -10,6 +10,7 @@ import com.newrelic.telemetry.Java11HttpPoster;
 import com.newrelic.telemetry.LogBatchSenderFactory;
 import com.newrelic.telemetry.MetricBatchSenderFactory;
 import com.newrelic.telemetry.OkHttpPoster;
+import com.newrelic.telemetry.SenderConfiguration;
 import com.newrelic.telemetry.SpanBatchSenderFactory;
 import com.newrelic.telemetry.TelemetryClient;
 import com.newrelic.telemetry.events.EventBatchSender;
@@ -27,11 +28,11 @@ public class ConfigurationExamples {
   public static void main(String[] args) throws MalformedURLException {
     String insertApiKey = args[0];
 
-    MetricBatchSender sender =
-        MetricBatchSender.builder()
+    SenderConfiguration config = MetricBatchSender.configurationBuilder()
             .apiKey(insertApiKey)
             .httpPoster(new OkHttpPoster(Duration.ofSeconds(2)))
             .build();
+    MetricBatchSender sender = MetricBatchSender.create(config);
     new TelemetryClient(sender, null, null, null);
 
     // new configuration methods:
