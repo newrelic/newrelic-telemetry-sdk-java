181c2073452dffddf6b5d225638884b974e2476b
Tony Mancill <tmancill@newrelic.com>
2022-08-27 00:19:25 +0000

    Update Utils.generateUUID() to generate UUIDs conforming to RFC 4122 V4

---
+++ telemetry-core/src/test/java/com/newrelic/telemetry/util/UtilsTest.java
@@ -0,0 +1,24 @@
+package com.newrelic.telemetry.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UtilsTest {
+    @Test
+    @DisplayName("Utils.generateUUID() generates V4 UUID")
+    void testGenerateUUID() throws Exception {
+        final UUID uuid = Utils.generateUUID();
+
+        assertEquals(4, uuid.version()); // version 4 as per RFC 4122
+        assertEquals(2, uuid.variant()); // variant 2 as per RFC 4122
+
+        // UUID.randomUUID() generates V4 UUIDs
+        final UUID referenceUUID = UUID.randomUUID();
+        assertEquals(referenceUUID.version(), uuid.version());
+        assertEquals(referenceUUID.variant(), uuid.variant());
+    }
+}
