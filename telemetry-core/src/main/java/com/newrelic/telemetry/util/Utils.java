93256a805e6da33de942f26c63cfe896ace3832f
tmancill <1195611+tmancill@users.noreply.github.com>
2022-08-29 14:29:10 -0700

    Update Utils.generateUUID() to generate UUIDs conforming to RFC 4122 V4 (#300)

--- telemetry-core/src/main/java/com/newrelic/telemetry/util/Utils.java
+++ telemetry-core/src/main/java/com/newrelic/telemetry/util/Utils.java
@@ -29,7 +29,28 @@ public class Utils {
     return verifyNonNull(input, "input cannot be null");
   }
 
+  /**
+   * Generate a random UUID using ThreadLocalRandom.
+   *
+   * @return a UUID conforming to V4 of RFC 4122
+   */
   public static UUID generateUUID() {
-    return new UUID(ThreadLocalRandom.current().nextLong(), ThreadLocalRandom.current().nextLong());
+    byte[] randomBytes = new byte[16];
+    ThreadLocalRandom.current().nextBytes(randomBytes);
+
+    // Conform to the behavior of UUID.randomUUID()
+    // version 4, variant 2: https://www.ietf.org/rfc/rfc4122.txt
+    randomBytes[6] &= 0x0f; /* clear version        */
+    randomBytes[6] |= 0x40; /* set to version 4     */
+    randomBytes[8] &= 0x3f; /* clear variant        */
+    randomBytes[8] |= 0x80; /* set to IETF variant  */
+
+    // UUID(byte[]) is private, so replicate the logic here to pack bytes into longs
+    long msb = 0;
+    long lsb = 0;
+    for (int i = 0; i < 8; i++) msb = (msb << 8) | (randomBytes[i] & 0xff);
+    for (int i = 8; i < 16; i++) lsb = (lsb << 8) | (randomBytes[i] & 0xff);
+
+    return new UUID(msb, lsb);
   }
 }
